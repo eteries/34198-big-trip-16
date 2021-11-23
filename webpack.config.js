ec840ebb2e97e0f7e72f912c77de773edce7ecf8
Яньшин Сергей Сергеевич <yanshin.s@medindex.ru>
2021-11-24 00:17:40 +0300

    feat(1.8): add babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,19 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
   devtool: 'source-map',
   devServer: {
     hot: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      }
+    ]
   }
 }
